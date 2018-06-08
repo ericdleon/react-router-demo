33e76e9ec823177b256444d785cc59f932cec9d5
Eric <deleoneric97@gmail.com>
2018-06-08 18:43:07 -0400

    test

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const Topics = ({ match }) => (
 )
 
 const BasicExample = () => (
-  <Router basename = {process.env.PUBLIC_URL}>
+  <Router basename = {process.env.PUBLIC_URL + '/'}>
     <div>
       <ul>
         <li><Link to="/">Home</Link></li>
